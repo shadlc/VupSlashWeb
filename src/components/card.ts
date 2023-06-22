18d1f3029db1c2956b354991d863e160b26f49a6
似然可绘 <i@shadlc.net>
2023-06-22 10:58:20 +0800

    Fix Gitee Page

--- src/components/card.ts
+++ src/components/card.ts
@@ -139,7 +139,7 @@ export function setParty(card: Card, parties: any) {
   parties.forEach((p: any) => {
     if (p.code == card.party) {
       card.importLogo(
-        "/assets/img/logo/" + card.party + ".png",
+        "assets/img/logo/" + card.party + ".png",
         card
       );
       card.themeColor = p.themeColor ?? card.themeColor;
@@ -152,7 +152,7 @@ export function setParty(card: Card, parties: any) {
 
 export function setShadow(card: Card) {
   card.importShadow(
-    "/assets/img/shadow/" + card.shadowType + ".png"
+    "assets/img/shadow/" + card.shadowType + ".png"
   );
 }
 
